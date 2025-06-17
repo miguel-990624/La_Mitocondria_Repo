461cbc353a21b1ba2e2ea2ae41711c24538745d7
miguel-990624 <miguelangelmolinagutierrez@gmail.com>
2025-06-17 13:37:27 -0500

    Added the third button as well as the html file where it is implemented

--- app.js
+++ app.js
@@ -8,27 +8,21 @@ const randomizedMessage = () => {
     switch (randomNumber){
         case 1:
             messagesDisplay.innerHTML = messagesList[0];
-            console.log(randomNumber);
             break;
         case 2:
             messagesDisplay.innerHTML = messagesList[1];
-            console.log(randomNumber);
             break;
         case 3:
             messagesDisplay.innerHTML = messagesList[2];
-            console.log(randomNumber);
             break;
         case 4:
             messagesDisplay.innerHTML = messagesList[3];
-            console.log(randomNumber);
             break;
         case 5:
             messagesDisplay.innerHTML = messagesList[4];
-            console.log(randomNumber);
             break;
         default:
             messagesDisplay.innerHTML = "Oops, looks like something went wrong, Try again."
-            console.log(randomNumber);
             break;
     }
 };
