e838d4db622e88940bdf5922219d49a41c622f42
Jhongun69 <jhonvilla.2000@hotmail.com>
2025-06-19 10:06:18 -0500

    Carga de archivos, boton 1, Jhonse

--- app.js
+++ app.js
@@ -1,4 +1,44 @@
-let soundclick = new Audio("./assets/Click.wav")
+//Backgroung Music
+const startButton = document.getElementById("startGame");
+const bgMusic = document.getElementById("bgMusic");
+const volumeControl = document.getElementById("volumeControl");
+
+startButton.addEventListener("click", () => {
+    bgMusic.volume = volumeControl.value;
+    bgMusic.play();
+});
+
+volumeControl.addEventListener("input", () => {
+    bgMusic.volume = volumeControl.value;
+});
+//End Backgroung Music
+
+//Button 1
+const greetingButton = document.getElementById("greetingButton");
+const greetingDisplay = document.getElementById("greetingDisplay");
+
+function getGreetingMessage() {
+    const currentHour = new Date().getHours(); // Obtiene la hora actual (0-23)
+    //Crea un objeto de fecha actual y extrae la hora
+    if (currentHour < 12) {//Si la hora es antes de medio dia
+        return "¡Good morning!";
+    } else if (currentHour < 18) {//si es entre las 12 y las 6
+        return "¡Good evening!";
+    } else {//si es despues de las 6 
+        return "¡Good night!";
+    }
+}
+
+greetingButton.addEventListener("click", () => {
+    const message = getGreetingMessage();
+    greetingDisplay.textContent = message;
+
+    soundclick.currentTime = 0;
+    soundclick.play();
+});
+
+//Button 2
+let soundclick = new Audio("./assets/Click_Counter.wav")
 //Inicializo contador en 0
 let counter = 0;
 let counterButton = document.getElementById("counterButton"); //Conecto el ID de HTML
@@ -7,7 +47,7 @@ let increase = document.getElementById('increase')
 let decrease = document.getElementById('decrease')
 let reset = document.getElementById('reset')
 
-//Creo las funciones de cada button
+//I create the functions of each button
 increase.addEventListener('click', function () {
     counter += 1;
     counterButton.innerText = counter;
@@ -39,11 +79,71 @@ reset.addEventListener('click', function () {
     soundclick.play()
 });
 
-//función actualizar Color
+//update Color function
 function changeColor() {
     if (counter > 0) {
-        counterButton.style.color = '#00b8d0';
+        counterButton.style.color = '#00ffea';
     } else {
         counterButton.style.color = 'white';
     }
 }
+
+
+
+// button 3
+const messagesDisplay = document.getElementById("messagesDisplay")
+const messagesButton = document.getElementById("messagesButton")
+
+const randomizedMessage = () => {
+    const messagesList = ["Hello World", "Bye World", "s'up World", "Fuck you World", "World"];
+    const randomNumber = Math.ceil(Math.random() * 5);
+    switch (randomNumber) {
+        case 1:
+            messagesDisplay.innerHTML = messagesList[0];
+            break;
+        case 2:
+            messagesDisplay.innerHTML = messagesList[1];
+            break;
+        case 3:
+            messagesDisplay.innerHTML = messagesList[2];
+            break;
+        case 4:
+            messagesDisplay.innerHTML = messagesList[3];
+            break;
+        case 5:
+            messagesDisplay.innerHTML = messagesList[4];
+            break;
+        default:
+            messagesDisplay.innerHTML = "Oops, looks like something went wrong, Try again."
+            break;
+    }
+};
+
+messagesButton.addEventListener("click", (e) => {
+    randomizedMessage();
+    soundclick.currentTime = 0;
+    soundclick.play()
+});
+
+//button 5
+
+const colorDisplay = document.getElementById("colorDisplay")
+const colorButton = document.getElementById("colorButton")
+let colorCounter = 0;
+
+const colorDisplayChange = () => {
+    const colorList = ["red", "orange", "yellow", "lightGreen", "aquamarine", "blue", "royalBlue", "purple", "fuchsia"]
+    if (colorCounter === (colorList.length - 1)) {
+        colorCounter = 0;
+    } else {
+        colorCounter += 1;
+    }
+    colorDisplay.className = colorList[colorCounter]
+
+};
+
+colorButton.addEventListener("click", (e) => {
+    colorDisplayChange();
+    soundclick.currentTime = 0;
+    soundclick.play()
+});
